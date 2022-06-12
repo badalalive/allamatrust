a639683bc864f9643b24339eef81c69cae9eb281
badal gupta <badal965484@gmail.com>
2022-06-12 21:22:01 +0530

    chage navbar

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//     // alert("hello"); 
+//     $(".items li a").click( function(e) {
+//         let target = $(this).attr("href");
+//         $("html.body").animate({
+//             screenTop:$(target).offset().top;
+//         }, 100);
+//         e.preventDefault();
+//     })
+// });
