b57e9ccac9a79b1a771e6948b7d1a04f2f2c3450
badal gupta <badal965484@gmail.com>
2022-06-21 20:34:45 +0530

    major changes

--- js/index.js
+++ js/index.js
@@ -8,3 +8,24 @@
 //         e.preventDefault();
 //     })
 // });
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      autoplay: true,
+      margin: 5,
+      responsive:{
+          0:{
+              items:1,
+              nav:true
+          },
+          600:{
+              items:3,
+              nav:false
+          },
+          1000:{
+              items:3,
+              nav:true,
+              loop:false
+          }
+      }
+  });
+  });
